68337f03c4ba32f3c3f545aef288d9a278443ee9
erichvorster <erichv@infoslips.com>
2023-01-31 16:17:51 +0200

    made request and response

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
-
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log('request made')
-})
+  console.log(req);
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for request on port 3000')
-})
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  // send an HTML file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+    //   res.write(data);
+    
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening for request on port 3000");
+});
